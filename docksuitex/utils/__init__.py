73fc518a93f4ee9cb283e67270dc8dcc3531359f
MangalamGautamSinha <mangalamgsinha@gmail.com>
2026-02-16 12:42:16 +0530

    updated init

--- docksuitex/utils/__init__.py
+++ docksuitex/utils/__init__.py
@@ -1,6 +1,5 @@
 from .viewer import view_molecule, view_docked_poses
 from .fetcher import fetch_pdb, fetch_sdf
-from .cleaner import delete_binaries
 from .parser import parse_vina_log_to_csv, parse_ad4_dlg_to_csv
 
 __all__ = [
@@ -10,5 +9,4 @@ __all__ = [
     "fetch_sdf",
     "parse_vina_log_to_csv",
     "parse_ad4_dlg_to_csv",
-    "delete_binaries"
 ]
